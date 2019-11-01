37e099ef7219c454c8fed9a3548b148d6e992964
GABRIEL AUGUSTO NUNES NOBREGA DINIZ <gaobaofogo@A309-37L.imd.local>
2019-11-01 19:09:56 -0300

    Adicionei arquivo .c

---
+++ trabalho.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  trabalho.c
+ *
+ *    Description:  Treino para o github
+ *
+ *        Version:  1.0
+ *        Created:  01/11/2019 19:06:14
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Gabriel, 
+ *   Organization:  UFRN
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+
+int main(){
+
+  printf("Hello world\n");
+
+  return 0;
+}
